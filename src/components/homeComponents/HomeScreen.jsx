eee409936ed4eb37506690c5e90262838191ae06
Mudpadans <philmodel.101@gmail.com>
2023-08-01 22:42:58 -0400

    heads & feet

--- src/components/homeComponents/HomeScreen.jsx
+++ src/components/homeComponents/HomeScreen.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 import AdBanner from './AdBanner'
 
-const HomeScreen = () => {  
+const HomeScreen = () => { 
+  const [recipes, setRecipes] = useState([])
+  
+  const getRecipes = () => {
+    axios 
+      .get("https://recipes.devmountain.com/recipes")
+      .then((res) => {
+        setRecipes(res.data)
+        console.log(res.data)
+      })
+  }
+
+  useEffect(() => {
+    getRecipes()
+  }, [])
+
   return (
     <div>
       <AdBanner />
