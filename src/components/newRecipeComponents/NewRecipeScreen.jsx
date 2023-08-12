512ea030c40195a34ad9f34e1560056e25a1ac04
Mudpadans <philmodel.101@gmail.com>
2023-08-12 17:22:32 -0400

    more form stuff

--- src/components/newRecipeComponents/NewRecipeScreen.jsx
+++ src/components/newRecipeComponents/NewRecipeScreen.jsx
@@ -12,30 +12,42 @@ const NewRecipeScreen = () => {
     instructions: "",
   };
 
-  
+  const onSubmit = (values) => {
+    console.log(values)
+  }
 
   return (
     <section>
       <h1>Tell us about your Recipe!</h1>
         <form>
-          <label></label>
+          <label for="radio-buttons">
+            <label for="cook">Cook
+              <input type="radio" name="cook"></input>
+            </label>
+            <label for="bake">Bake
+              <input type="radio" name="bake"></input>
+            </label>
+            <label for="drink">Drink
+              <input type="radio" name="drink"></input>
+            </label>
+          </label>
+          <label>Recipe Name</label>
           <input />
-          <label></label>
+          <label>Image URL</label>
           <input />
-          <label></label>
+          <label>Prep Time</label>
           <input />
-          <label></label>
+          <label>Cook Time</label>
           <input />
-          <label></label>
+          <label>Number of People Served</label>
           <input />
-          <label></label>
+          <label>Ingredients</label>
           <input />
-          <label></label>
+          <label>Quantity</label>
           <input />
-          <button type="radio"></button>
-          <button type="radio"></button>
-          <button type="radio"></button>
+          <label>Instructions</label>
           <textarea></textarea>
+          <button>Submit</button>
         </form>
     </section>
   );
